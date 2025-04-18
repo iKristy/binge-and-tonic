478a7df8265b132fcf7f65e04e2430ccdf184ac3
Kristy Marcinova <kristy.m@me.com>
2025-04-18 16:40:18 +0100

    Change toast color

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -27,7 +27,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "bg-primary text-primary-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
