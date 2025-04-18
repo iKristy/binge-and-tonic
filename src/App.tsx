e1046e128a98bd76244d453bd5887d897e3f3db6
Kristy Marcinova <kristy.m@me.com>
2025-04-18 19:35:34 +0100

    Remove unneeded stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -9,7 +8,6 @@ import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 import RequireAuth from "./components/RequireAuth";
-import IconGenerator from "./pages/IconGenerator";
 
 const queryClient = new QueryClient();
 
@@ -28,7 +26,6 @@ const App = () => (
                   <Index />
                 </RequireAuth>
               } />
-              <Route path="/icon-generator" element={<IconGenerator />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
--- src/pages/IconGenerator.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import GeneratePWAIcons from '@/components/icons/GeneratePWAIcons';
-
-const IconGenerator = () => {
-  return <GeneratePWAIcons />;
-};
-
-export default IconGenerator;
