b7cac9102fa71ded646952cabb66421840031f76
Kristy Marcinova <kristy.m@me.com>
2025-04-18 04:55:25 +0100

    Overhaul badges

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -16,9 +16,9 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-destructive-foreground",
         outline: "text-foreground",
         complete:
-          "border-transparent bg-primary/80 text-primary-foreground border-white/20 backdrop-blur-md",
+          "bg-primary/80 text-primary-foreground border-white/20 backdrop-blur-md",
         inProgress:
-          "border-transparent bg-orange-500/80 text-background border-orange-100/20 backdrop-blur-md",
+          "bg-black/80 text-orange-500 border-orange-500/30 backdrop-blur-md",
       },
     },
     defaultVariants: {
