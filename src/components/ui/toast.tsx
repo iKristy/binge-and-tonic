a3e9e5feef1c1720200f60ad68961628e6fb44b2
Kristy Marcinova <kristy.m@me.com>
2025-04-18 16:41:01 +0100

    Update toast

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -27,7 +27,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground",
+        default: "bg-blue-700/90 backdrop-blur-sm text-primary-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
