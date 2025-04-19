452302120063da7d6650bac146b44b2eef307703
Kristy Marcinova <kristy.m@me.com>
2025-04-19 02:47:27 +0100

    Update badge styles

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -16,9 +16,9 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-destructive-foreground",
         outline: "text-foreground",
         complete:
-          "bg-blue-700/90 text-foreground border-blue-500/30",
+          "bg-blue-700/80 text-foreground border-blue-500/50",
         inProgress:
-          "bg-orange-400/90 text-background border-orange-300/30",
+          "bg-orange-400/80 text-background border-orange-300/50",
       },
     },
     defaultVariants: {
